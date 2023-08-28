ccf7978f65f96ae589174b38971d76e303935408
runawaycake <st72x500y@gmail.com>
2023-08-28 20:16:22 +0800

    settings.py database

--- blackrabbit/settings.py
+++ blackrabbit/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'blackrabbit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'data',                       
-        'USER': 'root',
-        'PASSWORD': 'AmaneKanata',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
